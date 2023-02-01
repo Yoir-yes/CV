4312fd73657864a332190d330458db31a9980e0a
Ernest <ernesterni456@gmail.com>
2023-02-01 22:58:55 +0100

    Oh yeah

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 
 let teraz = document.getElementById('gen')
-for (let i = 1; i < 51; i++) {
+for (let i = 1; i < 40; i++) {
     let numer = i
     let span = document.createElement('span')
     span.innerHTML =  "<span class='numer'>"+numer+"</span>"
     parent = document.getElementById('gen')
     parent.appendChild(span)
 }
+var i = 0;
+var txt = 'Ernest Sapijaszko CV';
+var speed = 150;
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
